d6d8d8f83bb4551aaf28d0ba6d503f055b605020
HEAVE8N <108592942+HEAVE8N@users.noreply.github.com>
2026-03-19 21:04:20 +0500

    Update Program.fs

--- Lab 2.1/Lab 2.1/Program.fs
+++ Lab 2.1/Lab 2.1/Program.fs
@@ -3,12 +3,12 @@
 let getMinDigits (input: string) =
     input.Split(' ')
 
-    |> Array.toList
+    |> Array.toList // в первой тоже мейби нахуй
     |> List.filter (fun s -> 
         s <> "" && s |> Seq.forall Char.IsDigit && int s > 0)
 
     |> List.map (fun s -> 
-        s |> Seq.map (fun c -> int c - int '0') |> Seq.min)
+        s |> Seq.map (fun c -> int c - int '0') |> Seq.min) // может но не хочет, мейби
 
 [<EntryPoint>]
 let main _ =
